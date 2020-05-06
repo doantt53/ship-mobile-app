1d55949a88023d8b59aed32c61ad1ad0f8441f24
Tranngocquy <gemq289x@gmail.com>
2020-05-06 09:28:37 +0700

    Update Map

--- lib/ui/lockedscreen/HomePage.dart
+++ lib/ui/lockedscreen/HomePage.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../../constants.dart';
@@ -7,6 +10,30 @@ import '../../data/models/auth.dart';
 import '../app/app_drawer.dart';
 
 class HomePage extends StatelessWidget {
+
+  Completer<GoogleMapController> _Controller = Completer();
+
+  static const LatLng _center =
+  const LatLng(20.845724105834961, 106.71234893798828);
+  static const LatLng _center1 =
+  const LatLng(20.845735549926758, 106.71233367919922);
+  static const LatLng _center2 = const LatLng(20.842907, 106.709155);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition0 = _center;
+  LatLng _lastMapPosition1 = _center1;
+  LatLng _lastMapPosition2 = _center2;
+  MapType _currentMaptype = MapType.normal;
+
+
+  _onMapCreated(GoogleMapController controller) {
+    _Controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition0 = position.target;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final _auth = Provider.of<AuthModel>(context, listen: true);
@@ -24,58 +51,19 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(height: 10.0),
-            _auth?.user?.avatar != null
-                ? Center(
-              child: Container(
-                width: 120.0,
-                height: 120.0,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  image: DecorationImage(
-                    image: NetworkImage(_auth?.user?.avatar),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(60.0)),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 2.0,
-                  ),
-                ),
-              ),
-            )
-                : Container(
-              height: 0.0,
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 7.0,
             ),
-            ListTile(
-              title: Text('ID'),
-              subtitle: _auth?.user?.id == null
-                  ? null
-                  : Text(
-                _auth?.user?.id.toString() ?? "",
-              ),
-            ),
-            ListTile(
-              title: Text('First Name'),
-              subtitle: _auth?.user?.firstname == null
-                  ? null
-                  : Text(
-                _auth?.user?.firstname.toString() ?? "",
-              ),
-            ),
-            ListTile(
-              title: Text('Last Name'),
-              subtitle: _auth?.user?.lastname == null
-                  ? null
-                  : Text(
-                _auth?.user?.lastname.toString() ?? "",
-              ),
-            ),
-          ],
-        ),
+            mapType: _currentMaptype,
+            markers: _markers,
+            onCameraMove: _onCameraMove,
+          )
+        ],
       ),
     );
   }
